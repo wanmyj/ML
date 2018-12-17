f89a20ec4709b5a0f9d3c24f4ca8baf3b8931150
wanmyj <wanmyj@hotmail.com>
2018-12-17 21:44:39 +0800

    upload 3 mi add class

--- python_test/temparature_transfer.py
+++ python_test/temparature_transfer.py
@@ -1,6 +1,9 @@
 ## temparature transfer
 
 #tempConvert.py
+''' multiline 
+comment'''
+
 TempStr = input("please input a temparature value with symbol:")
 if TempStr[-1] in ['F', 'f']:
     C = (eval(TempStr[0:-1]) - 32)/1.8
@@ -10,3 +13,13 @@ elif TempStr[-1] in ['C', 'c']:
     print("the converted temp is {:.2f}F".format(F))
 else:
     print("invalid input")
+
+
+
+TempStr = input()
+if TempStr[1:4] in ['RMB']:
+    C = eval(TempStr[4:]/6.78
+    print("USD{:.2f}".format(C))
+elif TempStr[1:4] in ['USD']:
+    F = eval(TempStr[4:]*6.78
+    print("RMB{:.2f}".format(F))
